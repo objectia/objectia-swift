f4f64a1020cb2b9ca5db3a46494b4fcf95e90543
8dos <8dos@local>
2019-11-28 14:14:22 +0200

    Getting ready for release

--- Sources/Objectia/Constants.swift
+++ Sources/Objectia/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public struct Constants {
     public static let API_BASE_URL: String = "https://api.objectia.com"
-    public static let VERSION: String = "0.9.2"
+    public static let VERSION: String = "1.0.0"
     public static let USER_AGENT: String = "objectia-swift/" + VERSION
     public static let DEFAULT_TIMEOUT: TimeInterval = 30.0 // seconds
 }
